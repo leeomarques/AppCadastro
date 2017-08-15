d4f4761d7c4ec7f5717e371d3ba2fd8e46c5cb85
Leonardo Marques <leonardogm_16@hotmail.com>
2017-08-15 18:10:03 -0300

    Finalizando

--- app/src/main/java/com/example/leonardo/cadastro/MainActivity.java
+++ app/src/main/java/com/example/leonardo/cadastro/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.leonardo.cadastro;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button botao3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        botao3 = (Button) findViewById(R.id.btop3);
+
+        botao3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(MainActivity.this, StudentActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void onDisplay(View v){
@@ -26,5 +39,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btop1);
         Toast.makeText(MainActivity.this, "Seja Bem Vindo(a)!", Toast.LENGTH_LONG).show();
     }
+
+
 
 }
