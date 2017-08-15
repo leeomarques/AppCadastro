c7818620a9cf4de28bc6265b0725b2e181f816be
Leonardo Marques <leonardogm_16@hotmail.com>
2017-08-15 16:57:19 -0300

    Tres Butons com o Toast o Primeiro

--- app/src/main/java/com/example/leonardo/cadastro/MainActivity.java
+++ app/src/main/java/com/example/leonardo/cadastro/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.leonardo.cadastro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
+    }
+
+    public void onDisplay(View v){
+
+        findViewById(R.id.btop1);
+        Toast.makeText(MainActivity.this, "Utilize a Outra Opção", Toast.LENGTH_SHORT).show();
     }
 }
